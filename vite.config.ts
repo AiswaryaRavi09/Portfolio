94e2680b36776ef077e8fc23a62181ed687f814c
Aiswarya <aiswaryaravindran2@gmail.com>
2025-06-27 22:57:48 +0530

    my portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // 👈 This is the correct fix
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
